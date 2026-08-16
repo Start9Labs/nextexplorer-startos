a680f939cb375ac84904c4458a947fd944f37dbc
Helix <267227783+helix-nine@users.noreply.github.com>
2026-08-16 08:32:58 -0600

    Initial commit

---
+++ startos/manifest/index.ts
@@ -0,0 +1,21 @@
+import { setupManifest } from '@start9labs/start-sdk'
+import { long, short } from './i18n'
+
+export const manifest = setupManifest({
+  id: 'hello-world',
+  title: 'Hello World',
+  license: 'MIT',
+  packageRepo: 'https://github.com/Start9Labs/hello-world-startos',
+  upstreamRepo: 'https://github.com/Start9Labs/hello-world',
+  marketingUrl: 'https://start9.com/',
+  donationUrl: 'https://donate.start9.com/',
+  description: { short, long },
+  volumes: ['main'],
+  images: {
+    'hello-world': {
+      source: { dockerTag: 'ghcr.io/start9labs/hello-world:2.0.0' },
+      arch: ['x86_64', 'aarch64', 'riscv64'],
+    },
+  },
+  dependencies: {},
+})
---
+++ startos/versions/current.ts
@@ -0,0 +1,16 @@
+import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
+
+export const current = VersionInfo.of({
+  version: '2.0.0:9',
+  releaseNotes: {
+    en_US: 'Internal updates (start-sdk 2.0.x)',
+    es_ES: 'Actualizaciones internas (start-sdk 2.0.x)',
+    de_DE: 'Interne Aktualisierungen (start-sdk 2.0.x)',
+    pl_PL: 'Aktualizacje wewnętrzne (start-sdk 2.0.x)',
+    fr_FR: 'Mises à jour internes (start-sdk 2.0.x)',
+  },
+  migrations: {
+    up: async ({ effects }) => {},
+    down: IMPOSSIBLE,
+  },
+})
